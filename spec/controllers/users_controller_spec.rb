fbd7af2e7ba989633add45df7417cfc04a830acf
Pranav <pranav0073@gmail.com>
2015-01-25 00:08:50 +0530

    Finished layout and routes

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe UsersController do
+integrate_views
+  #Delete these examples and add some real ones
+  
+
+
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+
+
+  it "should have the right title" do
+    get 'new'
+    response.should have_tag("title", /Sign up/)
+  end
+end
+end
