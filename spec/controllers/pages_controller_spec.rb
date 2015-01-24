c6179525fbc1fe24314f0a1fcb76e2b280c065c6
Pranav <pranav0073@gmail.com>
2015-01-24 15:12:16 +0530

    added helper tilte for application

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
   
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -13,7 +17,7 @@ describe PagesController do
     it "should have the right tile" do
       get 'home'
       response.should have_tag("title",
-        "Ruby on Rails Tutorial Sample App | Home")
+        @base_title + "Home")
     end
   end
 
@@ -26,7 +30,7 @@ describe PagesController do
     it "should have the right tile" do
       get 'contact'
       response.should have_tag("title",
-        "Ruby on Rails Tutorial Sample App | Contact")
+        @base_title + "Contact")
     end
   end
 
@@ -39,7 +43,20 @@ describe PagesController do
     it "should have the right tile" do
       get 'about'
       response.should have_tag("title",
-        "Ruby on Rails Tutorial Sample App | About")
+        @base_title + "About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successfull" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title tag" do
+      get 'help'
+      response.should have_tag("title",
+        @base_title + "Help")
     end
   end
 end
