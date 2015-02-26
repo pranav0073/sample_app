e85709c912bd018dca9e654758db3b35216eab0c
Pranav <pranav0073@gmail.com>
2015-02-26 16:02:45 +0530

    connceted oracle 11g

--- vendor/plugins/audited-1.1.1/lib/acts_as_audited.rb
+++ vendor/plugins/audited-1.1.1/lib/acts_as_audited.rb
@@ -1,23 +1,4 @@
-# Copyright (c) 2006 Brandon Keepers
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 module CollectiveIdea #:nodoc:
   module Acts #:nodoc:
@@ -62,7 +43,7 @@ module CollectiveIdea #:nodoc:
         #     end
         #
         def acts_as_audited(options = {})
-          # don't allow multiple calls
+          # don't allow multiple calls    
           return if self.included_modules.include?(CollectiveIdea::Acts::Audited::InstanceMethods)
 
           options = {:protect => accessible_attributes.nil?}.merge(options)
@@ -98,71 +79,12 @@ module CollectiveIdea #:nodoc:
 
       module InstanceMethods
 
-        # Temporarily turns off auditing while saving.
-        def save_without_auditing
-          without_auditing { save }
-        end
-
-        # Executes the block with the auditing callbacks disabled.
-        #
-        #   @foo.without_auditing do
-        #     @foo.save
-        #   end
-        #
-        def without_auditing(&block)
-          self.class.without_auditing(&block)
-        end
-
-        # Gets an array of the revisions available
-        #
-        #   user.revisions.each do |revision|
-        #     user.name
-        #     user.version
-        #   end
-        #
-        def revisions(from_version = 1)
-          audits = self.audits.find(:all, :conditions => ['version >= ?', from_version])
-          return [] if audits.empty?
-          revision = self.audits.find_by_version(from_version).revision
-          Audit.reconstruct_attributes(audits) {|attrs| revision.revision_with(attrs) }
-        end
-
-        # Get a specific revision specified by the version number, or +:previous+
-        def revision(version)
-          revision_with Audit.reconstruct_attributes(audits_to(version))
-        end
-
-        def revision_at(date_or_time)
-          audits = self.audits.find(:all, :conditions => ["created_at <= ?", date_or_time])
-          revision_with Audit.reconstruct_attributes(audits) unless audits.empty?
-        end
 
         def audited_attributes
           attributes.except(*non_audited_columns)
         end
 
       public
-
-        def revision_with(attributes)
-          returning self.dup do |revision|
-            revision.send :instance_variable_set, '@attributes', self.attributes_before_type_cast
-            Audit.assign_revision_attributes(revision, attributes)
-
-            # Remove any association proxies so that they will be recreated
-            # and reference the correct object for this revision. The only way
-            # to determine if an instance variable is a proxy object is to
-            # see if it responds to certain methods, as it forwards almost
-            # everything to its target.
-            for ivar in revision.instance_variables
-              proxy = revision.instance_variable_get ivar
-              if !proxy.nil? and proxy.respond_to? :proxy_respond_to?
-                revision.instance_variable_set ivar, nil
-              end
-            end
-          end
-        end
-
-      
 
         def audited_changes
           changed_attributes.except(*non_audited_columns).inject({}) do |changes,(attr, old_value)|
@@ -171,49 +93,60 @@ module CollectiveIdea #:nodoc:
           end
         end
 
-        def audits_to(version = nil)
-          if version == :previous
-            version = if self.version
-              self.version - 1
-            else
-              previous = audits.find(:first, :offset => 1,
-                :order => "#{Audit.quoted_table_name}.version DESC")
-              previous ? previous.version : 1
-            end
-          end
-          audits.find(:all, :conditions => ['version <= ?', version])
-        end
+        
 
         def audit_create
-          write_audit(:action => 'create', :changes => audited_attributes)
+          write_audit_for_create(:action => 'create', :changes => audited_attributes)
         end
 
         def audit_update
           unless (changes = audited_changes).empty?
-            write_audit_for_update(:action => 'update', :changes => changes)
+            write_audit_for_update_or_destroy(:action => 'update', :changes => changes)
           end
         end
 
         def audit_destroy
-          write_audit(:action => 'destroy', :changes => audited_attributes)
+          write_audit_for_destroy(:action => 'destroy', :changes => audited_attributes)
         end
 
         def write_audit(attrs)
           self.audits.create attrs if auditing_enabled
         end
 
-        def write_audit_for_update(attrs)
+        def write_audit_for_update_or_destroy(attrs)
           temp = attrs[:changes]
           attrs.except!(:changes)
           temp.each do |key, array|
             attrs[:column_name] = key
             attrs[:old_value] = array[0]
             attrs[:new_value] = array[1]
+            self.audits.create attrs if auditing_enabled
           end
-
-          self.audits.create attrs if auditing_enabled
-
         end
+
+        def write_audit_for_create(attrs)
+          temp = attrs[:changes]
+          attrs.except!(:changes)
+          temp.each do |key, val_f|
+            attrs[:column_name] = key.to_s
+            attrs[:old_value] = ""
+            attrs[:new_value] = val_f
+            self.audits.create attrs if auditing_enabled
+          end
+        end
+
+        def write_audit_for_destroy(attrs)
+          temp = attrs[:changes]
+          attrs.except!(:changes)
+          temp.each do |key, val_f|
+            attrs[:column_name] = key.to_s
+            attrs[:old_value] = val_f
+            attrs[:new_value] = ""
+            self.audits.create attrs if auditing_enabled
+          end
+        end
+
+
       end # InstanceMethods
 
       module SingletonMethods
@@ -245,9 +178,6 @@ module CollectiveIdea #:nodoc:
         # All audit operations during the block are recorded as being
         # made by +user+. This is not model specific, the method is a
         # convenience wrapper around #Audit.as_user.
-        def audit_as( user, &block )
-          Audit.as_user( user, &block )
-        end
 
       end
     end
